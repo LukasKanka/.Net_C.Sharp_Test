17a25a02060a7c51d69dc47380656d8d99feae97
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-08-23 10:23:20 +0200

    firstTest NUnit selenium.dev

---
+++ SeleniumNunitProject/SeleniumNunitProject/Tests/SeleniumTests.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SeleniumNunitProject.Tests
+{
+    internal class SeleniumTests
+    {
+        [Test]
+
+        public void FirstSeleniumTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.selenium.dev/");
+
+            // Zkontrolujeme název Titulku
+            Assert.That(driver.Title, Is.EqualTo("Selenium"));
+
+            driver.Quit();
+
+        }
+    }
+}
