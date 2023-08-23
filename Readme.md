1e9640bf58089154fc9b1465538631aa836585f8
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-08-23 15:54:04 +0200

    nunit pořádek

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Rozcestník Lukáše Kaňky na tomto GitHub profilu
+
+**Něco málo o mě**
+Jsem IT nadšenec s pozitivním přístupem k životu a vášní pro technologie. Propadl jsem testingu, kde jsem začal s manuálním testováním, ale více mě baví nástroje umožňující automatizaci, především Playwright. Vzdělávám se v dalších frameworcích umožňující automatizaci:
+
+Selenium,
+
+Selenide + Java, 
+
+NUnit + C#,
+
+Cypress.
+
+**Kde všude mě najdete:**
+[LinkedIn](https://www.linkedin.com/in/luk%C3%A1%C5%A1-ka%C5%88ka-b2a0a1a0/)
---
+++ SeleniumNunitProject/SeleniumNunitProject/Tests/InteractionsTests.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumNunitProject.Tests
+{
+    internal class InteractionsTests
+    {
+        [Test]
+        public void InteractionsTest()
+        {
+            var results = new List<string>();
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+
+            // click  default checkbox
+            driver.FindElement(By.Id("my-check-2")).Click();
+
+            // click radio button
+            driver.FindElement(By.Id("my-radio-2")).Click();
+
+            // right click
+            var actions = new Actions(driver);
+            var button = driver.FindElement(By.TagName("button"));
+            actions.ContextClick(button).Perform();
+
+            // double click
+            var checkbox1 = driver.FindElement(By.Id("my-check-1"));
+            actions.DoubleClick(checkbox1).Perform();
+
+            // sendkeys input
+            driver.FindElement(By.Id("my-text-id")).SendKeys(Guid.NewGuid().ToString());
+
+            // text area
+            var textArea = driver.FindElement(By.Name("my-textarea"));
+            textArea.SendKeys(Guid.NewGuid().ToString());
+
+            // clear
+            //textArea.Clear();
+
+
+
+           //driver.Quit();
+
+        }
+    }
+}
