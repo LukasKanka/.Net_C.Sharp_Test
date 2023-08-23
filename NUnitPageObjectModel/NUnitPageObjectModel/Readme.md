e39f027f8e73be93bcc82e77320dc5cda953e497
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-08-23 09:48:32 +0200

    HomePage vid 2 9:30

--- NUnitPageObjectModel/NUnitPageObjectModel/Readme.md
+++ NUnitPageObjectModel/NUnitPageObjectModel/Readme.md
@@ -1,2 +1,11 @@
 **Projekt VisualStudio 2022**
 NUnit Page Object Model
+
+Založit nový NUnit projekt
+
+Do projektu do instalovat Packages: Selenium.WebDriver a Selenium.Support
+
+Dále je potøeba do vyhledávaní packages zadat: dotnetselenium a nainstalovat DotNetSeleniumExtras.PageObjects.Core
+
+Návod jak na POM:
+https://www.youtube.com/watch?v=Cr8oLGTSDB4&list=PL6SXxvjnlkaTNJu7G9ZpGpJGnhAFu79UZ&index=2
--- NUnitPageObjectModel/NUnitPageObjectModel/Source/Pages/HomePage.cs
+++ NUnitPageObjectModel/NUnitPageObjectModel/Source/Pages/HomePage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace NUnitPageObjectModel.Source.Pages
 {
-    internal class HomePage
-    {
+    public class HomePage
+    {   
+        private IWebDriver _driver;
+        public HomePage(IWebDriver driver)
+            
+        {
+            _driver = driver;
+        }
+
+        public void Search(string searchText) 
+        {
+            _driver.FindElement(By.Id("twoabsearchtextbox"));
+
+        }
     }
 }
