962794bdf19233bd4982013e8b5553745f46edb9
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-08-23 19:04:12 +0200

    Page Objects video 9 4:30 NUnit Visual Studio 2022

---
+++ SeleniumNunitProject/PageObjects/PageObjects/WebFormPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjects.PageObjects
+{
+    public class WebFormPage
+    {
+        // locators
+        IWebElement TextArea => driver.FindElement(By.Name("my-textarea"));
+
+
+        IWebDriver driver;
+
+        public WebFormPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        // methods
+        public WebFormPage WriteTextToTextArea(string text)
+        {
+            TextArea.SendKeys(text);
+            return this;
+        }
+    }
+}
