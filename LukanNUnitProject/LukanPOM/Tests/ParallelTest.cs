73802285dd389be55e8baa78f4f945d96da87853
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-08-31 22:12:09 +0200

    Parallerní spouštění testů

---
+++ LukanNUnitProject/LukanPOM/Tests/ParallelTest.cs
@@ -0,0 +1,25 @@
+﻿using LukanPOM.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukanPOM.Tests
+{
+    //[Parallelizable(ParallelScope.All)]
+    internal class ParallelTest : TestBase
+    {
+        [Test]
+        public void AcceptCookieTest()
+        {
+            Home.AcceptCookie();
+        }
+
+        [Test]
+        public void DeclineCookieTest()
+        {
+            HomeTwo.DeclineCookie2();
+        }
+    }
+}
