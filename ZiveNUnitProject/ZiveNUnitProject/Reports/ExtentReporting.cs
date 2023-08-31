60cfdc03835d49aff41e73dcbe58dc3cf0a7dae0
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-08-31 21:44:24 +0200

    report příprava

---
+++ ZiveNUnitProject/ZiveNUnitProject/Reports/ExtentReporting.cs
@@ -0,0 +1,21 @@
+﻿using AventStack.ExtentReports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZiveNUnitProject.Reports
+{
+    internal class ExtentReporting
+    {
+        private static ExtentReports extentReports;
+        private static ExtentTest extentTest;
+
+        private static ExtentReports StartReporting()
+        {
+            
+        }
+
+    }
+}
