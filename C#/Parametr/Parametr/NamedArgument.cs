be2640307c43ad4e672d3f64cf62fb19a17d1dfe
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-08-31 22:34:36 +0200

    parametr argument, C#/Parametr, Visual Studio 2022

---
+++ C#/Parametr/Parametr/NamedArgument.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parametr
+{
+   public class NamedArgument
+    {
+        public static void Main(string[] args)
+        {
+            PrintEmployeeDetail("Franta", 12, 2015);
+
+            PrintEmployeeDetail(age: 34, name: "Karel", empid: 2345); // tímto způsobem nemusíme dodržovat pořadí argumentů
+
+
+        }
+        static void PrintEmployeeDetail(string name, int age, int empid)
+        {
+            Console.WriteLine($"Name : {name}, Age : {age}, Employee ID : {empid}");
+        }
+    }
+    
+}
