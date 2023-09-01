4d16c152677fe9298290d7d5ad5681b3ee798b67
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-09-01 20:03:42 +0200

    C# ApiRest, vs2022

---
+++ RestApiTests/RestApiTests/TestFixtures/UsersTests.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RestApiTests.Models.Users;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiTests.TestFixtures
+{
+    internal class UsersTests
+    {
+        [Test]
+        public void GetUsers()
+        {
+            var endpoint = "https://reqres.in/api/users";
+            var client = new RestClient(endpoint);
+            var request = new RestRequest(endpoint);
+            request.Method = Method.Get;
+
+            var response = client.Execute(request);
+            var responseBody = 
+                JsonConvert.DeserializeObject<UsersResponse>(response.Content);
+                
+        }
+
+    }
+}
