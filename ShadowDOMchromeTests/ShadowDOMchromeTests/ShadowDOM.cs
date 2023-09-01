d9c2221ec02945ffeb18c541a5493dad6324c790
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-09-01 20:47:54 +0200

    shadow DOM (není hotové)

---
+++ ShadowDOMchromeTests/ShadowDOMchromeTests/ShadowDOM.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace ShadowDOMchromeTests
+{
+    public class ShadowDOM
+    {
+        [Test]
+        public void DomTest()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            ChromeDriver driver = new ChromeDriver();
+
+            IWebElement element = driver.FindElement(By.XPath("//settings-ui")).GetShadowRoot()
+                .FindElement(By.CssSelector("settings-main#main")).GetShadowRoot()
+                .FindElement(By.CssSelector(""))
+                https://www.youtube.com/watch?v=-uMLqBO2x7c&t=724s
+
+        }
+    }
+}
