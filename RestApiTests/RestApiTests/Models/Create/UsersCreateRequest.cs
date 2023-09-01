05f75328d06c206c5a496e4ad446baea8d162f72
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-09-01 23:01:48 +0200

    Api C#

---
+++ RestApiTests/RestApiTests/Models/Create/UsersCreateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiTests.Models.Create
+{
+    public class UsersCreateRequest
+    {
+        
+            public string name { get; set; }
+            public string job { get; set; }
+        
+    }
+}
--- RestApiTests/RestApiTests/TestFixtures/UsersTests.cs
+++ RestApiTests/RestApiTests/TestFixtures/UsersTests.cs
@@ -1,5 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using FluentAssertions;
+using Newtonsoft.Json;
 using NUnit.Framework;
+using RestApiTests.Models.Create;
 using RestApiTests.Models.Users;
 using RestSharp;
 using System;
@@ -12,7 +14,7 @@ namespace RestApiTests.TestFixtures
 {
     internal class UsersTests
     {
-        [Test]
+        [Test] // test zde spouštíme jako debug test (Aspon první fáze)
         public void GetUsers()
         {
             var endpoint = "https://reqres.in/api/users";
@@ -23,7 +25,39 @@ namespace RestApiTests.TestFixtures
             var response = client.Execute(request);
             var responseBody = 
                 JsonConvert.DeserializeObject<UsersResponse>(response.Content);
+
+            // Verify (fluent package)
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            responseBody.page.Should().Be(1);
+            responseBody.data.Should().NotBeNullOrEmpty();
+
                 
+        }
+
+        [Test] 
+        public void CreateUsers()
+        {
+            var endpoint = "https://reqres.in/api/users";
+            var client = new RestClient(endpoint);
+            var request = new RestRequest(endpoint);
+            request.Method = Method.Post;
+
+            var payload = new UsersCreateRequest
+            {
+                name = "John",
+                job = "Engineer"
+            };
+
+            request.AddJsonBody(payload);   
+
+            var response = client.Execute(request);
+            var responseBody =
+                JsonConvert.DeserializeObject<UsersCreateResponse>(response.Content);
+
+            // Verify (fluent package)
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+            responseBody.id.Should().NotBeNullOrEmpty();
+
         }
 
     }
